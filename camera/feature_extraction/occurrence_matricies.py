aa4aa8484eedfdf6cd43de058d112c481f2f589d
Alexander Sologub <alexsologoub@gmail.com>
2018-01-12 01:00:32 +0300

    Try to parallelize patch processing

--- camera/feature_extraction/occurrence_matricies.py
+++ camera/feature_extraction/occurrence_matricies.py
@@ -3,10 +3,14 @@ from camera.interpolation.metrics import difference
 from camera.interpolation.nearest_neighbour import interpolate_bayer
 
 def occurrence_matrix(image, quantization=2, threshold=3):
+    # TODO AS: We need interpolation for red channel only
     interpolated_image = interpolate_bayer(image)
     # TODO AS: Since diff is later used as index, make sure it is >= 0
+    # TODO AS: We need difference for red channel only
     diff = difference(image, interpolated_image, quantization=quantization, threshold=threshold) + threshold
-    red, green, blue = diff[::, ::, 0], diff[::, ::, 1], diff[::, ::, 2]
+    red = diff[::, ::, 0]
+    # green = diff[::, ::, 1]
+    # blue = diff[::, ::, 2]
     pattern_dim = 2 * threshold + 1
     patterns = np.zeros((pattern_dim, pattern_dim, pattern_dim))
     np.add.at(patterns, [red[::2, ::2], red[::2, 1::2], red[1::2, 1::2]], 1)
--- tasks.py
+++ tasks.py
@@ -1,36 +1,35 @@
 from invoke import task
 from camera.shared.data import get_all_labels, load_images
 from camera.feature_extraction.occurrence_matricies import occurrence_matrix
-from skimage.util import view_as_blocks
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
-from skimage import io
-from matplotlib import pyplot as plt
+from joblib import Parallel, delayed
+
+def process_patch(image, i, j, size, threshold):
+    patch = image[i * size:(i + 1) * size, j * size:(j + 1) * size]
+    return occurrence_matrix(patch, threshold).reshape(-1)
 
 @task
 def extr(ctx):
-    sample_id = 0
     image_id = 0
     patch_size = 512
+    threshold = 2
+    delayed_process_patch = delayed(process_patch)
 
-    for label in tqdm(get_all_labels()):
-        for image in tqdm(load_images(label)[:20]):
-            try:
-                image_patches = list()
-                if (image.shape == ()):
-                    # TODO AS: Some images have second thumbnail frame
-                    continue
-                for i in range(image.shape[0] // patch_size)[:4]:
-                    for j in range(image.shape[1] // patch_size)[:4]:
-                        patch = image[i * patch_size:(i + 1) * patch_size, j * patch_size:(j + 1) * patch_size]
-                        # TODO AS: Optimal threshold value
-                        features = np.append(occurrence_matrix(patch, threshold=2).reshape(-1), [image_id, label])
-                        image_patches.append(features)
-                        sample_id += 1
-                df = pd.DataFrame(image_patches)
-                df.to_csv('./extracted_features.csv', mode='a', header=False)
-            except Exception as err:
-                tqdm.write('Error on image {} with label {}: {}'.format(image_id, label, err))
-
-            image_id += 1
+    with Parallel(n_jobs=-1, backend='threading') as parallel:
+        for label in tqdm(get_all_labels()):
+            for image in tqdm(load_images(label)[:20]):
+                try:
+                    if (image.shape == ()):
+                        # TODO AS: Some images have second thumbnail frame
+                        continue
+                    for i in range(image.shape[0] // patch_size):
+                       image_patches = parallel(delayed_process_patch(image, i, j, patch_size, threshold) for j in range(image.shape[1] // patch_size))
+                    df = pd.DataFrame(image_patches)
+                    df['image_id'] = image_id
+                    df['label'] = label
+                    df.to_csv('./extracted_features.csv', mode='a', header=False)
+                except Exception as err:
+                    tqdm.write('Error on image {} with label {}: {}'.format(image_id, label, err))
+                image_id += 1
