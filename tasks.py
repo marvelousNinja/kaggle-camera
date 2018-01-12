bde8e4d4f06133982aa7fe2e1e2b9a26de1a29a5
Alexander Sologub <alexsologoub@gmail.com>
2018-01-12 15:01:40 +0300

    Isolate task code

--- tasks.py
+++ tasks.py
@@ -8,13 +8,19 @@ from joblib import Parallel, delayed
 
 def process_patch(image, i, j, size, threshold):
     patch = image[i * size:(i + 1) * size, j * size:(j + 1) * size]
-    return occurrence_matrix(patch, threshold).reshape(-1)
+    residual_dimension = 2 * threshold + 1
+    patterns = np.zeros((residual_dimension, residual_dimension, residual_dimension))
+    # TODO AS: Performance eater
+    np.add.at(patterns, [patch[::2, ::2], patch[::2, 1::2], patch[1::2, 1::2]], 1)
+    patterns /= (patch.shape[0] * patch.shape[1] / 4)
+    return patterns.reshape(-1)
 
 @task
 def extr(ctx):
     image_id = 0
-    patch_size = 512
+    size = 512
     threshold = 3
+    quantization = 2
     delayed_process_patch = delayed(process_patch)
 
     with Parallel(n_jobs=-1, backend='threading') as parallel:
@@ -24,14 +30,31 @@ def extr(ctx):
                     if (image.shape == ()):
                         # TODO AS: Some images have second thumbnail frame
                         continue
-                    for i in range(image.shape[0] // patch_size):
-                        img = image[:, :, 0].astype(np.int16)
-                        image_patches = parallel(delayed_process_patch(img, i, j, patch_size, threshold) for j in range(image.shape[1] // patch_size))
 
-                    df = pd.DataFrame(image_patches)
+                    # 1. Interpolate
+                    red_image = image[:, :, 0].astype(np.float)
+                    interpolated_image = np.array(red_image)
+                    true_red = red_image[1::2, 0::2]
+                    interpolated_image[0::2, 0::2] = true_red
+                    interpolated_image[1::2, 1::2] = true_red
+                    interpolated_image[0::2, 1::2] = true_red
+
+                    # 2. Calculate difference, quantize and clip
+                    diff = red_image - interpolated_image
+                    diff /= quantization
+                    diff = np.floor(diff).astype(np.int)
+                    # TODO AS: Performance eater
+                    np.clip(diff, a_min=-threshold, a_max=threshold, out=diff)
+
+                    # 3. For each patch, calculate co-occurrence matrix
+                    processed_patches = [delayed_process_patch(diff, i, j, size, threshold) for i in range(diff.shape[0] // size) for j in range(diff.shape[1] // size)]
+
+                    # 4. Append information to the csv
+                    df = pd.DataFrame(processed_patches)
                     df['image_id'] = image_id
                     df['label'] = label
                     df.to_csv('./extracted_features.csv', mode='a', header=False)
                 except Exception as err:
+                    raise err
                     tqdm.write('Error on image {} with label {}: {}'.format(image_id, label, err))
                 image_id += 1
