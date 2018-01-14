e4f67627e118aaeac14d9fc314071a4e89d65027
Alexander Sologub <alexsologoub@gmail.com>
2018-01-14 19:57:29 +0300

    Remove unused modules

--- camera/feature_extraction/occurrence_matricies_test.py
+++
@@ -1,9 +0,0 @@
-from camera.feature_extraction.occurrence_matricies import occurrence_matrix
-from camera.shared.data import load_sample_image
-import numpy as np
-
-def test_occurrence_matrix():
-    matrix = occurrence_matrix(load_sample_image())
-    assert matrix.shape == (5, 5, 5)
-    print(np.linalg.norm(matrix))
-    print(matrix)
