75885d5dd4b4d3249df81d03d3f2ae87609358f5
Alexander Sologub <alexsologoub@gmail.com>
2018-01-19 13:35:10 +0300

    Remove unused code

--- tasks.py
+++ tasks.py
@@ -9,191 +9,17 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from dotenv import load_dotenv, find_dotenv
 from camera.data import list_dirs_in, list_images_in
+from camera.generators import train_test_holdout_split
+from camera.generators import list_all_samples_in
+from camera.generators import image_generator, parallel_image_generator
+from camera.generators import center_crop
+from camera.generators import sequential_crop
+from camera.generators import random_crop
+from camera.feature_extraction import intra_channel
+from itertools import islice
+from multiprocessing import Pool
 
 load_dotenv(find_dotenv())
-
-@task
-def extract_intra_features(_):
-    image_id = 0
-    threshold = 3
-    quantization = 2
-    data_dir = os.environ['DATA_DIR']
-    output_dir = os.path.join(data_dir, 'intra_channel_features')
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    with open(output_dir + '/intra_channel_features.csv', 'a') as output_file:
-        for image_path in tqdm(list_images_in(data_dir + '/transformed_patches')):
-            image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
-
-            # 0. Extract information from filename
-            name = os.path.splitext(os.path.basename(image_path))[0]
-            label, image_id, transform, _ = name.split('_')
-
-            # 1. Interpolate
-            red_image = image[:, :, 0].astype(np.int16)
-            interpolated_image = np.array(red_image)
-            true_red = red_image[1::2, 0::2]
-            interpolated_image[0::2, 0::2] = true_red
-            interpolated_image[1::2, 1::2] = true_red
-            interpolated_image[0::2, 1::2] = true_red
-
-            # 2. Calculate difference, quantize and clip
-            diff = red_image - interpolated_image
-            diff //= quantization
-            # TODO AS: Performance eater
-            np.clip(diff, a_min=-threshold, a_max=threshold, out=diff)
-            diff += threshold
-
-            # 3. Calculate co-occurrence matrix
-            residual_dimension = 2 * threshold + 1
-            patterns = np.zeros((residual_dimension, residual_dimension, residual_dimension), dtype=np.int)
-            # TODO AS: Performance eater
-            np.add.at(patterns, [diff[::2, ::2], diff[::2, 1::2], diff[1::2, 1::2]], 1)
-            patterns = patterns / (diff.shape[0] * diff.shape[1] / 4)
-
-            # 4. Append information to the csv
-            features = np.append(patterns.reshape(-1), [transform != 'unalt', image_id, label])
-            np.savetxt(output_file, [features], delimiter=',', fmt='%s')
-
-@task
-def extract_inter_features(_):
-    threshold = 3
-    quantization = 2
-    data_dir = os.environ['DATA_DIR']
-    output_dir = os.path.join(data_dir, 'inter_channel_features')
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    with open(output_dir + '/inter_channel_features.csv', 'a') as output_file:
-        for image_path in tqdm(list_images_in(data_dir + '/transformed_patches')):
-            image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
-
-            # 0. Extract information from filename
-            name = os.path.splitext(os.path.basename(image_path))[0]
-            label, image_id, transform, _ = name.split('_')
-
-            # 1. Interpolate
-            red_green_image = image[:, :, :2].astype(np.int32)
-            interpolated_image = np.array(red_green_image)
-
-            true_red = red_green_image[1::2, 0::2, 0]
-            interpolated_image[0::2, 0::2, 0] = true_red
-            interpolated_image[1::2, 1::2, 0] = true_red
-            interpolated_image[0::2, 1::2, 0] = true_red
-
-            true_green_1 = red_green_image[0::2, 0::2, 1]
-            true_green_2 = red_green_image[1::2, 1::2, 1]
-            interpolated_image[0::2, 1::2, 1] = true_green_1
-            interpolated_image[1::2, 0::2, 1] = true_green_2
-
-            # 2. Calculate difference, quantize and clip
-            diff = red_green_image - interpolated_image
-            diff //= quantization
-            # TODO AS: Performance eater
-            np.clip(diff, a_min=-threshold, a_max=threshold, out=diff)
-            diff += threshold
-
-            # 3. Calculate co-occurrence matrix
-            residual_dimension = 2 * threshold + 1
-            patterns = np.zeros((residual_dimension, residual_dimension, residual_dimension), dtype=np.int)
-            # TODO AS: Performance eater
-            np.add.at(patterns, [diff[::2, ::2, 0], diff[::2, 1::2, 0], diff[0::2, 1::2, 1]], 1)
-            np.add.at(patterns, [diff[1::2, 1::2, 0], diff[::2, 1::2, 0], diff[0::2, 1::2, 1]], 1)
-            patterns = patterns / (diff.shape[0] * diff.shape[1] / 4)
-
-            # 4. Append information to the csv
-            features = np.append(patterns.reshape(-1), [transform != 'unalt', image_id, label])
-            np.savetxt(output_file, [features], delimiter=',', fmt='%s')
-
-@task
-def predict(_):
-    data_dir = os.environ['DATA_DIR']
-
-    intra_channel_features = pd.read_csv(data_dir + '/intra_channel_features/intra_channel_features.csv', header=None)
-    inter_channel_features = pd.read_csv(data_dir + '/inter_channel_features/inter_channel_features.csv', header=None)
-
-    image_id_column = inter_channel_features.columns[-2]
-    target_column = inter_channel_features.columns[-1]
-
-    image_id = inter_channel_features[image_id_column]
-    target = inter_channel_features[target_column]
-
-    intra_channel_features.drop([image_id_column, target_column], axis=1, inplace=True)
-    inter_channel_features.drop([image_id_column, target_column], axis=1, inplace=True)
-
-    full_data = pd.concat([intra_channel_features, inter_channel_features], axis=1)
-
-    full_data['zero_count'] = (full_data == 0).astype(int).sum(axis=1)
-    full_data['zero_count'] = (full_data == 0).astype(int).sum(axis=1)
-
-    train_mask = pd.factorize(image_id)[0] % 10 != 0
-
-    X_train, y_train = full_data[train_mask], target[train_mask]
-    X_test, y_test = full_data[~train_mask], target[~train_mask]
-
-    params = {
-        'n_estimators': 10
-    }
-
-    model = RandomForestClassifier(**params)
-    model.fit(X_train, y_train)
-    predictions = model.predict(X_test)
-    print(classification_report(y_test, predictions))
-
-from camera.transforms import adjust_gamma
-from camera.transforms import flip_horizontally
-from camera.transforms import jpeg_compress
-from camera.transforms import resize
-from camera.transforms import crop_center
-
-def process_image(label, path, output_dir):
-    size = 512
-    image_id = name = os.path.splitext(os.path.basename(path))[0]
-    image = cv2.imread(path)
-
-    transforms = {
-        'unalt': lambda image: image,
-        'flip': flip_horizontally,
-        'gamma80': lambda image: adjust_gamma(image, 0.8),
-        'gamma120': lambda image: adjust_gamma(image, 1.2),
-        'jpeg70': lambda image: jpeg_compress(image, 70),
-        'jpeg90': lambda image: jpeg_compress(image, 90),
-        'resize50': lambda image: resize(image, 0.5),
-        'resize80': lambda image: resize(image, 0.8),
-        'resize150': lambda image: resize(image, 1.5),
-        'resize200': lambda image: resize(image, 2.0)
-    }
-
-    for transform_name, transform in transforms.items():
-        # TODO AS: Alternative cropping strategies
-        patch_id = 0
-        patch = crop_center(transform(image), size)
-        filename = f'{label}_{image_id}_{transform_name}_{patch_id}.png'
-        cv2.imwrite(os.path.join(output_dir, filename), patch)
-
-@task
-def transform_and_crop(_):
-    data_dir = os.environ['DATA_DIR']
-    output_dir = os.path.join(data_dir, 'transformed_patches')
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    labels_and_paths = []
-    for label in list_dirs_in(data_dir + '/train'):
-        paths = list_images_in(data_dir + '/train/' + label)
-        labels = [label] * len(paths)
-        labels_and_paths.extend(zip(labels, paths))
-
-    pool = Pool()
-    progress = tqdm(total=len(labels_and_paths))
-
-    for label, path in labels_and_paths:
-        pool.apply_async(process_image, args=(label, path, output_dir), callback=lambda _: progress.update())
-
-    pool.close()
-    pool.join()
-    pool.close()
 
 @task
 def download(ctx):
@@ -209,30 +35,17 @@ def download(ctx):
         ctx.run(f'unzip sample_submission.csv.zip -d {data_dir}')
         ctx.run('rm -f test.zip train.zip sample_submission.csv.zip')
 
-from lightgbm import LGBMClassifier
-from camera.generators import train_test_holdout_split
-from camera.generators import list_all_samples_in
-from camera.generators import image_generator, parallel_image_generator
-from camera.generators import center_crop
-from camera.generators import sequential_crop
-from camera.generators import random_crop
-from camera.feature_extraction import inter_channel
-from camera.feature_extraction import intra_channel
-from camera.feature_extraction import gram_matrix
-from itertools import islice
-from multiprocessing import Pool
-
 @task
-def generate_test(ctx):
+def generate_test(_):
     data_dir = os.environ['DATA_DIR']
     labels_and_image_paths = list_all_samples_in(data_dir)
     _, test, _ = train_test_holdout_split(labels_and_image_paths)
     crop_size = 512
 
     test_data = list()
-    for image_id, label, transform_name, image in tqdm(image_generator(test, crop_generator=center_crop, crop_size=crop_size), total=len(test)):
+    for _, label, transform_name, image in tqdm(islice(parallel_image_generator(Pool(), test, crop_generator=center_crop, crop_size=crop_size), len(test)), total=len(test)):
         features = np.concatenate([
-            inter_channel(image),
+            intra_channel(image),
             [int(transform_name != 'unalt'), label]
         ])
 
@@ -242,7 +55,7 @@ def generate_test(ctx):
     np.savetxt(data_dir + '/test.csv', full_test, fmt='%s', delimiter=',')
 
 @task
-def generate_datasets(ctx):
+def learn(_):
     data_dir = os.environ['DATA_DIR']
     labels_and_image_paths = list_all_samples_in(data_dir)
     train, test, holdout = train_test_holdout_split(labels_and_image_paths)
@@ -253,16 +66,16 @@ def generate_datasets(ctx):
 
     i = 0
     train_data = list()
-    for image_id, label, transform_name, image in tqdm(parallel_image_generator(Pool(), train, crop_generator=sequential_crop, crop_size=crop_size, crop_limit=15)):
+    for _, label, transform_name, image in tqdm(parallel_image_generator(Pool(), train, crop_generator=sequential_crop, crop_size=crop_size, crop_limit=15)):
         features = np.concatenate([
-            inter_channel(image),
+            intra_channel(image),
             [int(transform_name != 'unalt'), label]
         ])
 
         train_data.append(features)
 
         i += 1
-        if i % 100 == 0:
+        if i % 500 == 0:
             model = RandomForestClassifier(n_estimators=50, n_jobs=-1)
             full_train = np.array(train_data)
             X_train, y_train = full_train[:, :-1], full_train[:, -1]
