8c355d8a1fb553f6acf752a9e3c257a3a3dcd7b4
Alexander Sologub <alexsologoub@gmail.com>
2018-01-19 13:22:48 +0300

    Correct setup script

--- setup.sh
+++ setup.sh
@@ -10,6 +10,13 @@ sudo apt-get install -y unzip make build-essential \
 
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 
+# Setup pyenv
+export PYENV_ROOT="${HOME}/.pyenv"
+echo "export PATH=\"${PYENV_ROOT}/bin:\$PATH\"" >> ~/.profile
+echo "eval \"\$(pyenv init -)\"" >> ~/.profile
+echo "eval \"\$(pyenv virtualenv-init -)\"" >> ~/.profile
+source ~/.profile
+
 # Install Python and project deps
 pyenv install 3.6.3
 pyenv virtualenv 3.6.3 kaggle-camera-3.6.3
