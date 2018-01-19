5945e84325befb428cbf53445fb6b2d121025011
Alexander Sologub <alexsologoub@gmail.com>
2018-01-19 13:03:28 +0300

    Add parallel image iterator

--- camera/generators/__init__.py
+++ camera/generators/__init__.py
@@ -8,6 +8,19 @@ from camera.transforms import flip_horizontally
 from camera.transforms import jpeg_compress
 from camera.transforms import resize
 
+def intensity_texture_score(image):
+    image = image / 255
+    flat_image = image.reshape(-1, 3)
+    channel_mean = flat_image.mean(axis=0)
+    channel_std = flat_image.std(axis=0)
+
+    channel_mean_score = -4 * channel_mean ** 2 + 4 * channel_mean
+    channel_std_score = 1 - np.exp(-2 * np.log(10) * channel_std)
+
+    channel_mean_score_aggr = channel_mean_score.mean()
+    channel_std_score_aggr = channel_std_score.mean()
+    return 0.7 * channel_mean_score_aggr + 0.3 * channel_std_score_aggr
+
 def list_all_samples_in(data_dir):
     labels_and_image_paths = []
 
@@ -35,12 +48,20 @@ def center_crop(image, size, _):
     return [image[top_x:top_x + size, top_y:top_y + size]]
 
 def sequential_crop(image, size, limit):
+    threshold = 0.5
+    patches = []
     for i in range(image.shape[0] // size):
         for j in range(image.shape[1] // size):
-            if (i * (image.shape[1] // size) + j) >= limit:
-                return
+            patches.append(image[i * size:(i + 1) * size, j * size:(j + 1) * size])
 
-            yield image[i * size:(i + 1) * size, j * size:(j + 1) * size]
+    patch_scores = []
+    for patch in patches:
+        patch_scores.append(intensity_texture_score(patch))
+
+    scored_score_indicies = np.argsort(patch_scores)[::-1]
+    sorted_scores = np.array(patch_scores)[scored_score_indicies]
+    sorted_patches = np.array(patches)[scored_score_indicies]
+    return sorted_patches[sorted_scores >= 0.5][:limit]
 
 def random_crop(image, size, limit):
     x_max, y_max = image.shape[0] - size, image.shape[1] - size
@@ -48,21 +69,20 @@ def random_crop(image, size, limit):
         x, y = np.random.randint(0, x_max), np.random.randint(0, y_max)
         yield image[x:x + size, y:y + size]
 
-def image_generator(labels_and_image_paths, crop_generator=random_crop, crop_size=512, seed=11):
+def image_generator(labels_and_image_paths, crop_generator=random_crop, crop_size=512, crop_limit=15, seed=11):
     transforms = {
         'unalt': lambda image: image,
-        # 'gamma80': lambda image: adjust_gamma(image, 0.8),
-        # 'gamma120': lambda image: adjust_gamma(image, 1.2),
-        # 'jpeg70': lambda image: jpeg_compress(image, 70),
-        # 'jpeg90': lambda image: jpeg_compress(image, 90),
-        # 'resize50': lambda image: resize(image, 0.5),
-        # 'resize80': lambda image: resize(image, 0.8),
-        # 'resize150': lambda image: resize(image, 1.5),
-        # 'resize200': lambda image: resize(image, 2.0)
+        'gamma80': lambda image: adjust_gamma(image, 0.8),
+        'gamma120': lambda image: adjust_gamma(image, 1.2),
+        'jpeg70': lambda image: jpeg_compress(image, 70),
+        'jpeg90': lambda image: jpeg_compress(image, 90),
+        'resize50': lambda image: resize(image, 0.5),
+        'resize80': lambda image: resize(image, 0.8),
+        'resize150': lambda image: resize(image, 1.5),
+        'resize200': lambda image: resize(image, 2.0)
     }
 
-    # transform_weights = np.array([9, 1, 1, 1, 1, 1, 1, 1, 1])
-    transform_weights = np.array([1])
+    transform_weights = np.array([9, 1, 1, 1, 1, 1, 1, 1, 1])
     transform_weights = transform_weights / sum(transform_weights)
     transform_names = list(transforms.keys())
 
@@ -72,5 +92,43 @@ def image_generator(labels_and_image_paths, crop_generator=random_crop, crop_siz
         transform = transforms[transform_name]
         transformed_image = cv2.cvtColor(transform(cv2.imread(image_path)), cv2.COLOR_BGR2RGB)
         # image_id, label, altered, image
-        for patch in crop_generator(transformed_image, crop_size, 10):
+        for patch in crop_generator(transformed_image, crop_size, crop_limit):
             yield image_id, label, transform_name, patch
+
+
+from multiprocessing import Queue, Manager
+
+def process_label_image_path_pair(label, image_path, crop_generator, crop_size, crop_limit, queue):
+    transforms = {
+        'unalt': lambda image: image,
+        'gamma80': lambda image: adjust_gamma(image, 0.8),
+        'gamma120': lambda image: adjust_gamma(image, 1.2),
+        'jpeg70': lambda image: jpeg_compress(image, 70),
+        'jpeg90': lambda image: jpeg_compress(image, 90),
+        'resize50': lambda image: resize(image, 0.5),
+        'resize80': lambda image: resize(image, 0.8),
+        'resize150': lambda image: resize(image, 1.5),
+        'resize200': lambda image: resize(image, 2.0)
+    }
+
+    transform_weights = np.array([9, 1, 1, 1, 1, 1, 1, 1, 1])
+    transform_weights = transform_weights / sum(transform_weights)
+    transform_names = list(transforms.keys())
+
+    image_id = os.path.splitext(os.path.basename(image_path))[0]
+    transform_name = np.random.choice(transform_names, p=transform_weights)
+    transform = transforms[transform_name]
+    transformed_image = cv2.cvtColor(transform(cv2.imread(image_path)), cv2.COLOR_BGR2RGB)
+    # image_id, label, transform_name, image
+    for patch in crop_generator(transformed_image, crop_size, crop_limit):
+        queue.put((image_id, label, transform_name, patch))
+
+def parallel_image_generator(pool, labels_and_image_paths, crop_generator=random_crop, crop_size=512, crop_limit=15):
+    manager = Manager()
+    queue = manager.Queue(400)
+
+    for label, image_path in labels_and_image_paths:
+        pool.apply_async(process_label_image_path_pair, (label, image_path, crop_generator, crop_size, crop_limit, queue))
+
+    while True:
+        yield queue.get()
--- tasks.py
+++ tasks.py
@@ -212,7 +212,7 @@ def download(ctx):
 from lightgbm import LGBMClassifier
 from camera.generators import train_test_holdout_split
 from camera.generators import list_all_samples_in
-from camera.generators import image_generator
+from camera.generators import image_generator, parallel_image_generator
 from camera.generators import center_crop
 from camera.generators import sequential_crop
 from camera.generators import random_crop
@@ -220,41 +220,50 @@ from camera.feature_extraction import inter_channel
 from camera.feature_extraction import intra_channel
 from camera.feature_extraction import gram_matrix
 from itertools import islice
+from multiprocessing import Pool
 
 @task
-def generate_datasets(ctx):
+def generate_test(ctx):
     data_dir = os.environ['DATA_DIR']
     labels_and_image_paths = list_all_samples_in(data_dir)
-    train, test, holdout = train_test_holdout_split(labels_and_image_paths)
-    crop_size = 64
+    _, test, _ = train_test_holdout_split(labels_and_image_paths)
+    crop_size = 512
 
     test_data = list()
-    for image_id, label, transform_name, image in tqdm(islice(image_generator(test, crop_generator=center_crop, crop_size=crop_size), 200), total=200):
+    for image_id, label, transform_name, image in tqdm(image_generator(test, crop_generator=center_crop, crop_size=crop_size), total=len(test)):
         features = np.concatenate([
             inter_channel(image),
-            intra_channel(image),
             [int(transform_name != 'unalt'), label]
         ])
 
         test_data.append(features)
 
     full_test = np.array(test_data)
+    np.savetxt(data_dir + '/test.csv', full_test, fmt='%s', delimiter=',')
+
+@task
+def generate_datasets(ctx):
+    data_dir = os.environ['DATA_DIR']
+    labels_and_image_paths = list_all_samples_in(data_dir)
+    train, test, holdout = train_test_holdout_split(labels_and_image_paths)
+    crop_size = 512
+
+    full_test = np.loadtxt(data_dir + '/test.csv', delimiter=',', dtype=np.object)
     X_test, y_test = full_test[:, :-1], full_test[:, -1]
 
     i = 0
     train_data = list()
-    for image_id, label, transform_name, image in tqdm(image_generator(train, crop_generator=random_crop, crop_size=crop_size)):
+    for image_id, label, transform_name, image in tqdm(parallel_image_generator(Pool(), train, crop_generator=sequential_crop, crop_size=crop_size, crop_limit=15)):
         features = np.concatenate([
             inter_channel(image),
-            intra_channel(image),
             [int(transform_name != 'unalt'), label]
         ])
 
         train_data.append(features)
 
         i += 1
-        if i % 1000 == 0:
-            model = RandomForestClassifier(n_estimators=50)
+        if i % 100 == 0:
+            model = RandomForestClassifier(n_estimators=50, n_jobs=-1)
             full_train = np.array(train_data)
             X_train, y_train = full_train[:, :-1], full_train[:, -1]
             model.fit(X_train, y_train)
