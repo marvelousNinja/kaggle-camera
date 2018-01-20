e7246e7519680fd0101c1db61e94022bb5dd6c1f
Alexander Sologub <alexsologoub@gmail.com>
2018-01-20 16:47:02 +0300

    Reorder imports and delete old image generator

--- camera/generators/__init__.py
+++ camera/generators/__init__.py
@@ -1,6 +1,7 @@
+import os
+from multiprocessing import Queue, Manager
 import numpy as np
 import cv2
-import os
 from camera.data import list_dirs_in
 from camera.data import list_images_in
 from camera.transforms import adjust_gamma
@@ -61,42 +62,13 @@ def sequential_crop(image, size, limit):
     scored_score_indicies = np.argsort(patch_scores)[::-1]
     sorted_scores = np.array(patch_scores)[scored_score_indicies]
     sorted_patches = np.array(patches)[scored_score_indicies]
-    return sorted_patches[sorted_scores >= 0.5][:limit]
+    return sorted_patches[sorted_scores >= threshold][:limit]
 
 def random_crop(image, size, limit):
     x_max, y_max = image.shape[0] - size, image.shape[1] - size
     for _ in range(limit):
         x, y = np.random.randint(0, x_max), np.random.randint(0, y_max)
         yield image[x:x + size, y:y + size]
-
-def image_generator(labels_and_image_paths, crop_generator=random_crop, crop_size=512, crop_limit=15, seed=11):
-    transforms = {
-        'unalt': lambda image: image,
-        'gamma80': lambda image: adjust_gamma(image, 0.8),
-        'gamma120': lambda image: adjust_gamma(image, 1.2),
-        'jpeg70': lambda image: jpeg_compress(image, 70),
-        'jpeg90': lambda image: jpeg_compress(image, 90),
-        'resize50': lambda image: resize(image, 0.5),
-        'resize80': lambda image: resize(image, 0.8),
-        'resize150': lambda image: resize(image, 1.5),
-        'resize200': lambda image: resize(image, 2.0)
-    }
-
-    transform_weights = np.array([9, 1, 1, 1, 1, 1, 1, 1, 1])
-    transform_weights = transform_weights / sum(transform_weights)
-    transform_names = list(transforms.keys())
-
-    for label, image_path in labels_and_image_paths:
-        image_id = os.path.splitext(os.path.basename(image_path))[0]
-        transform_name = np.random.choice(transform_names, p=transform_weights)
-        transform = transforms[transform_name]
-        transformed_image = cv2.cvtColor(transform(cv2.imread(image_path)), cv2.COLOR_BGR2RGB)
-        # image_id, label, altered, image
-        for patch in crop_generator(transformed_image, crop_size, crop_limit):
-            yield image_id, label, transform_name, patch
-
-
-from multiprocessing import Queue, Manager
 
 def process_label_image_path_pair(label, image_path, crop_generator, crop_size, crop_limit, queue):
     transforms = {
--- tasks.py
+++ tasks.py
@@ -1,23 +1,19 @@
 import os
+from itertools import islice
+from multiprocessing import Pool
 from invoke import task
-import cv2
 from tqdm import tqdm
 import numpy as np
-from multiprocessing import Pool
-import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from dotenv import load_dotenv, find_dotenv
-from camera.data import list_dirs_in, list_images_in
 from camera.generators import train_test_holdout_split
 from camera.generators import list_all_samples_in
-from camera.generators import image_generator, parallel_image_generator
+from camera.generators import parallel_image_generator
 from camera.generators import center_crop
 from camera.generators import sequential_crop
 from camera.generators import random_crop
 from camera.feature_extraction import intra_channel
-from itertools import islice
-from multiprocessing import Pool
 
 load_dotenv(find_dotenv())
 
@@ -55,10 +51,10 @@ def generate_test(_):
     np.savetxt(data_dir + '/test.csv', full_test, fmt='%s', delimiter=',')
 
 @task
-def learn(_):
+def experiment(_):
     data_dir = os.environ['DATA_DIR']
     labels_and_image_paths = list_all_samples_in(data_dir)
-    train, test, holdout = train_test_holdout_split(labels_and_image_paths)
+    train, _, _ = train_test_holdout_split(labels_and_image_paths)
     crop_size = 512
 
     full_test = np.loadtxt(data_dir + '/test.csv', delimiter=',', dtype=np.object)
@@ -75,7 +71,7 @@ def learn(_):
         train_data.append(features)
 
         i += 1
-        if i % 500 == 0:
+        if i % 5000 == 0:
             model = RandomForestClassifier(n_estimators=50, n_jobs=-1)
             full_train = np.array(train_data)
             X_train, y_train = full_train[:, :-1], full_train[:, -1]
