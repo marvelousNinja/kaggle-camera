79a5f8d95217774268deec2808e49638e7b4a15c
Alexander Sologub <alexsologoub@gmail.com>
2018-01-25 23:09:05 +0300

    Use less data for residual experiment

--- camera/experiments/residual.py
+++ camera/experiments/residual.py
@@ -7,24 +7,42 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from camera.generators import image_generator, in_batches
 from camera.data import list_all_samples_in, train_test_holdout_split
-from camera.transforms import random_transform, center_crop, sequential_crop, default_transforms_and_weights, identity
+from camera.transforms import random_transform, center_crop, sequential_crop, default_transforms_and_weights, identity, flip_horizontally
 
 def process_image(quantization, threshold, img):
     image, label = img[0], img[1]
-
-    # 1. Convert to grayscale
-    grayscale_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    grayscale_image = grayscale_image.astype(np.float)
+    # # 1. Convert to single channel
+    # grayscale_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # grayscale_image = grayscale_image.astype(np.float)
+    grayscale_image = image[:, :, 1].astype(np.float)
 
     # 2. Calculate residuals
-    spam_11_filter = np.array([
+    # spam11 filter 3x3
+    filter = np.array([
         [-1, +2, -1],
         [+2, -4, +2],
         [-1, +2, -1]
     ])
 
-    diff = cv2.filter2D(grayscale_image, -1, spam_11_filter).astype(np.int)
+    # spam11 filter 5x5
+    filter = np.array([
+        [-1, +2, -2, +2, -1],
+        [+2, -6, +8, -6, +2],
+        [-2, +8, -12, +8, -2],
+        [+2, -6, +8, -6, +2],
+        [-1, +2, -2, +2, -1]
+    ])
+
+    # spam14v
+    # filter = np.array([
+    #     [-1, +2, -1],
+    #     [+2, -4, +2],
+    #     [+0, +0, +0]
+    # ])
+
+    diff = cv2.filter2D(grayscale_image, -1, filter).astype(np.int)
     diff //= quantization
+    diff = diff.astype(np.int)
     np.clip(diff, a_min=-threshold, a_max=threshold, out=diff)
     diff += threshold
 
@@ -33,42 +51,41 @@ def process_image(quantization, threshold, img):
     patterns = np.zeros((dim, dim, dim, dim), dtype=np.int)
     np.add.at(patterns, [diff[:, ::4], diff[:, 1::4], diff[:, 2::4], diff[:, 3::4]], 1)
     np.add.at(patterns, [diff[::4, :], diff[1::4, :], diff[2::4, :],  diff[3::4, :]], 1)
-    return np.concatenate([patterns.reshape(-1), [label]])
+    patterns = patterns.reshape(-1)
+    patterns = patterns / (len(diff[::4, :]) * 2)
+    return np.concatenate([patterns[:150], [label]])
 
 def conduct(data_dir):
+    quantization = 12
+    threshold = 2
+    crop_size = 512
+
     all_samples = list_all_samples_in(os.path.join(data_dir, 'train'))
     train, test, _ = train_test_holdout_split(all_samples)
 
     transforms_and_weights = default_transforms_and_weights()
 
-    validation_data = np.stack(list(map(partial(process_image, 1, 2), tqdm(image_generator(
-        test,
+    validation_data = np.stack(list(map(partial(process_image, quantization, threshold), tqdm(image_generator(
+        test[:100],
         identity, #partial(random_transform, transforms_and_weights),
-        partial(center_crop, 512),
+        partial(center_crop, crop_size),
         loop=False
     )))), axis=0)
 
     X_test = validation_data[:, :-1]
     y_test = validation_data[:, -1]
 
-    training_data = map(partial(process_image, 1, 2), tqdm(image_generator(
-        train,
+    training_data = np.stack(list(map(partial(process_image, quantization, threshold), tqdm(image_generator(
+        train[:1000],
         identity, #partial(random_transform, transforms_and_weights),
-        partial(sequential_crop, 512, 25),
+        partial(center_crop, crop_size),
         loop=False
-    )))
+    )))), axis=0)
 
-    X = list()
-    y = list()
+    X_train = training_data[:, :-1]
+    y_train = training_data[:, -1]
 
-    i = 0
-    for record in training_data:
-        X.append(record[:-1])
-        y.append(record[-1])
-        i += 1
-
-        if i % 5000 == 0:
-            model = RandomForestClassifier(n_estimators=50)
-            model.fit(X, y)
-            predictions = model.predict(X_test)
-            tqdm.write(classification_report(y_test, predictions))
+    model = RandomForestClassifier(n_estimators=50)
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    print(classification_report(y_test, predictions))
