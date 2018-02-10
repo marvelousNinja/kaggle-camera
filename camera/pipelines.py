8a05dfe79987e0ab55773d1b4ad8d1acd9a59262
Alexander Sologub <alexsologoub@gmail.com>
2018-02-10 12:56:31 +0300

    Adjust imports in pipelines and extract tta code

--- camera/pipelines.py
+++ camera/pipelines.py
@@ -1,17 +1,25 @@
 from functools import partial
+
 import numpy as np
-from camera.utils import pipe, evolve_at, transform_to_sample_weight
-from camera.data import read_jpeg, read_jpeg_cached, read_png
-from camera.transforms import (
-    crop_center, random_transform, default_transforms_and_weights,
-    crop_random, image_filters, random_flip, spam_11_5,
-    crop_top_left, crop_top_right, crop_bottom_left, crop_bottom_right
-)
+
+from camera.data import read_image
+from camera.data import read_image_cached
+from camera.transforms import crop_center
+from camera.transforms import crop_random
+from camera.transforms import crop_top_left
+from camera.transforms import crop_top_right
+from camera.transforms import crop_bottom_left
+from camera.transforms import crop_bottom_right
+from camera.transforms import default_transforms_and_weights
+from camera.transforms import image_filters
+from camera.transforms import random_flip
+from camera.transforms import random_transform
+from camera.utils import transform_to_sample_weight
 
 def training_pipeline(cache, image_filter, allow_flips, allow_weights, crop_size, record):
     path, label = record
     outer_crop_size = 780
-    image = read_jpeg_cached(cache, partial(crop_center, outer_crop_size), path)
+    image = read_image_cached(cache, partial(crop_center, outer_crop_size), path)
     if allow_flips: image = random_flip(image)
     image, transform_name = random_transform(default_transforms_and_weights(), image)
     image = crop_random(crop_size, image)
@@ -22,7 +30,7 @@ def training_pipeline(cache, image_filter, allow_flips, allow_weights, crop_size
 def validation_pipeline(image_filter, allow_weights, crop_size, record):
     path, label = record
     outer_crop_size = 512
-    image = read_jpeg(path)
+    image = read_image(path)
     image = crop_center(outer_crop_size, image)
     image, transform_name = random_transform(default_transforms_and_weights(), image)
     image = crop_center(crop_size, image)
@@ -33,42 +41,25 @@ def validation_pipeline(image_filter, allow_weights, crop_size, record):
 def tta_pipeline(image_filter, allow_weights, crop_size, record):
     path, label = record
     outer_crop_size = 512
-    image = read_jpeg(path)
+    image = read_image(path)
     image = crop_center(outer_crop_size, image)
     image, transform_name = random_transform(default_transforms_and_weights(), image)
     sample_weight = transform_to_sample_weight(transform_name)
-
-    image = image.astype(np.float32)
-    flipped_image = np.fliplr(image)
-
-    tta_crops = [
-        crop_top_left(crop_size, image),
-        crop_top_right(crop_size, image),
-        crop_bottom_left(crop_size, image),
-        crop_bottom_right(crop_size, image),
-        crop_center(crop_size, image),
-        crop_top_left(crop_size, flipped_image),
-        crop_top_right(crop_size, flipped_image),
-        crop_bottom_left(crop_size, flipped_image),
-        crop_bottom_right(crop_size, flipped_image),
-        crop_center(crop_size, flipped_image),
-    ]
-
+    tta_crops = get_tta_crops(crop_size, image)
     tta_crops = list(map(image_filters()[image_filter], tta_crops))
     return [tta_crops, label, sample_weight if allow_weights else 1.0]
 
-def submission_pipeline(image_filter, crop_size, path):
-    image = read_png(path)
-    image = crop_center(crop_size, image)
-    image = image_filters()[image_filter](image.astype(np.float32))
-    return image
-
 def tta_submission_pipeline(image_filter, crop_size, path):
-    image = read_png(path)
+    image = read_image(path)
+    tta_crops = get_tta_crops(crop_size, image)
+    tta_crops = list(map(image_filters()[image_filter], tta_crops))
+    return tta_crops
+
+def get_tta_crops(crop_size, image):
     image = image.astype(np.float32)
     flipped_image = np.fliplr(image)
 
-    tta_crops = [
+    return [
         crop_top_left(crop_size, image),
         crop_top_right(crop_size, image),
         crop_bottom_left(crop_size, image),
@@ -80,6 +71,3 @@ def tta_submission_pipeline(image_filter, crop_size, path):
         crop_bottom_right(crop_size, flipped_image),
         crop_center(crop_size, flipped_image),
     ]
-
-    tta_crops = list(map(image_filters()[image_filter], tta_crops))
-    return tta_crops
